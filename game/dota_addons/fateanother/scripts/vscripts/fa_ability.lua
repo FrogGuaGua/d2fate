7f0517f8da89f25bbfc4e93470c5e46c7496c923
Spectrum <mattkleroy@gmail.com>
2017-07-22 22:53:16 -0700

    FA fix
    W now puts camera  to where you counter to rather than Where you
    countered from

--- game/dota_addons/fateanother/scripts/vscripts/fa_ability.lua
+++ game/dota_addons/fateanother/scripts/vscripts/fa_ability.lua
@@ -141,6 +141,7 @@ function OnHeartDamageTaken(keys)
 		Timers:CreateTimer(function()
 			if counter == keys.AttackCount or not caster:IsAlive() then return end 
 			caster:PerformAttack( target, true, true, true, true, false, false, false )
+			caster:AddNewModifier(caster, caster, "modifier_camera_follow", {duration = 1.0})
 			CreateSlashFx(caster, target:GetAbsOrigin()+RandomVector(500), target:GetAbsOrigin()+RandomVector(500))
 			counter = counter+1
 			return 0.1
