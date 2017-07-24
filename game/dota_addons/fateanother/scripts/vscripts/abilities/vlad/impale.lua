0ca0c96c9e1c2b44090273445cc33a3280e9ebec
zx52 <xkamiess@gmail.com>
2017-07-24 20:40:23 +0200

    add impale to kv and remove dummy

--- game/dota_addons/fateanother/scripts/vscripts/abilities/vlad/impale.lua
+++ game/dota_addons/fateanother/scripts/vscripts/abilities/vlad/impale.lua
@@ -3,24 +3,24 @@ vlad_impale = class({})
 if not IsServer() then
   return
 end
-function vlad_impale:VFX1_SpikesIndicator(target,radius)
-  self.PI1 = FxCreator("particles/custom/vlad/vlad_ip_prespike.vpcf", PATTACH_ABSORIGIN, target,0,nil)
-  ParticleManager:SetParticleControlEnt(self.PI1, 3, target, PATTACH_CUSTOMORIGIN_FOLLOW	, nil, target:GetAbsOrigin(),false)
+function vlad_impale:VFX1_SpikesIndicator(caster,radius,point)
+  self.PI1 = ParticleManager:CreateParticle("particles/custom/vlad/vlad_ip_prespike.vpcf", PATTACH_CUSTOMORIGIN, caster)
+  ParticleManager:SetParticleControl(self.PI1,0,point)
+  ParticleManager:SetParticleControl(self.PI1,3,point)
   ParticleManager:SetParticleControl(self.PI1,4,Vector(radius, 0, 0))
 end
 function vlad_impale:VFX2_OnTargetImpale(k,target)
-  --bleed
   self.PI2[k] = FxCreator("particles/custom/vlad/vlad_impale_bleed.vpcf", PATTACH_ABSORIGIN_FOLLOW, target, 0, nil)
   ParticleManager:SetParticleControlEnt(self.PI2[k], 1, target, PATTACH_ABSORIGIN_FOLLOW	, nil, target:GetAbsOrigin(), false)
-  --ontarget impale
+  
   self.PI3[k] = FxCreator("particles/custom/vlad/vlad_kb_ontarget.vpcf", PATTACH_ABSORIGIN, target, 0, nil)
   ParticleManager:SetParticleControl(self.PI3[k],4, Vector(2.7, 0, 0))
 end
 --[[actually on a second thought this particle triggers me so much i would rather reuse R ontarget stuff
-function vlad_impale:VFX3_Spikes(target,radius)
-  self.PI3 = FxCreator("particles/custom/vlad/vlad_ip_spikes.vpcf", PATTACH_ABSORIGIN, target, 0, nil)
-  ParticleManager:SetParticleControl(self.PI3,4,Vector(radius, 0, 0))
-  --ParticleManager:SetParticleControlEnt(self.PI2, 1, target, PATTACH_ABSORIGIN	, nil, target:GetAbsOrigin(), false)
+function vlad_impale:VFX3_Spikes(caster,radius,point)
+  self.PI4 = ParticleManager:CreateParticle("particles/custom/vlad/vlad_ip_spikes.vpcf", PATTACH_CUSTOMORIGIN, caster)
+  ParticleManager:SetParticleControl(self.PI4,0,point + Vector(0,0,100))
+  ParticleManager:SetParticleControl(self.PI4,4,Vector(radius, 0, 0))
 end--]]
 
 function vlad_impale:OnUpgrade()
@@ -42,8 +42,22 @@ function vlad_impale:ResetImpaleSwapTimer()
   end
 end
 
+function vlad_impale:GetCastRange(vLocation,hTarget)
+  return self:GetSpecialValueFor("range")
+end
+
 function vlad_impale:OnSpellStart()
   local caster = self:GetCaster()
+  local stun_min = self:GetSpecialValueFor("stun_min")
+  local stun_gain = self:GetSpecialValueFor("stun_gain")
+  local stun_max = self:GetSpecialValueFor("stun_max")
+  local radius_min = self:GetSpecialValueFor("radius_min")
+  local radius_gain = self:GetSpecialValueFor("radius_gain")
+  local radius_max = self:GetSpecialValueFor("radius_max")
+  local damage = self:GetSpecialValueFor("damage")
+  local delay = self:GetSpecialValueFor("delay")
+  local point = caster:GetCursorPosition()
+  
   caster:RemoveModifierByName("modifier_transfusion_self")
   self:ResetImpaleSwapTimer()
 
@@ -51,34 +65,26 @@ function vlad_impale:OnSpellStart()
  	local bloodpower = modifier and modifier:GetStackCount() or 0
   caster:RemoveModifierByName("modifier_transfusion_bloodpower")
 
-  local stun = math.max(1, math.min(1 + bloodpower * 0.1, 2))
-  local damage = 200
-  local radius = math.max(150, math.min(150 + bloodpower * 5, 250))
-  print(stun, "   ", radius)
-  local point = caster:GetCursorPosition()
+  local stun = math.max(stun_min, math.min(stun_min + bloodpower * stun_gain, stun_max))
+  local radius = math.max(radius_min, math.min(radius_min + bloodpower * radius_gain, radius_max))
+  --print(stun, "   ", radius)
+    
+  self:VFX1_SpikesIndicator(caster,radius,point)
 
-  local dummy = CreateUnitByName("visible_dummy_unit", caster:GetOrigin(), false, caster, caster, caster:GetTeamNumber())
-  dummy:FindAbilityByName("dummy_visible_unit_passive"):SetLevel(1)
-  dummy:SetDayTimeVisionRange(0)
-  dummy:SetNightTimeVisionRange(0)
-  dummy:SetAbsOrigin(point)
-  self:VFX1_SpikesIndicator(dummy,radius)
-
-  Timers:CreateTimer(0.5, function()
+  Timers:CreateTimer(delay, function()
     self.PI2={}
     self.PI3={}
-    --self:VFX3_Spikes(dummy,radius)
-    local targets = FindUnitsInRadius(caster:GetTeamNumber(), dummy:GetAbsOrigin(), nil, radius, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_ALL, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, FIND_ANY_ORDER, false)
+    --self:VFX3_Spikes(caster,radius,point)
+    local targets = FindUnitsInRadius(caster:GetTeamNumber(), point, nil, radius, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_ALL, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, FIND_ANY_ORDER, false)
     for k,v in pairs(targets) do
       self:VFX2_OnTargetImpale(k,v)
-      DoDamage(caster, v, 1, DAMAGE_TYPE_MAGICAL, 0, self, false)
+      DoDamage(caster, v, damage, DAMAGE_TYPE_MAGICAL, 0, self, false)
       caster:AddBleedStack(v, 5,false)
       giveUnitDataDrivenModifier(caster, v, "stunned", stun)
-      --ApplyAirborne(caster, v, stun)
     end
     if #targets ~= 0 then
-      dummy:EmitSound("Hero_PhantomAssassin.CoupDeGrace")
-      dummy:EmitSound("Hero_Leshrac.Split_Earth")
+      targets[1]:EmitSound("Hero_PhantomAssassin.CoupDeGrace")
+      targets[1]:EmitSound("Hero_Leshrac.Split_Earth")
     end
   end)
 
@@ -86,13 +92,12 @@ function vlad_impale:OnSpellStart()
     FxDestroyer(self.PI1, false)
     FxDestroyer(self.PI2,false)
     FxDestroyer(self.PI3,false)
-    dummy:RemoveSelf()
+    --FxDestroyer(self.PI4,false)
   end)
 end
 
-
 function vlad_impale:GetCastAnimation()
-  return nil
+  return ACT_DOTA_CAST_ABILITY_3
 end
 
 function vlad_impale:GetAbilityTextureName()
