ccba6338e10d3b2e68549af315b61c3669e7dfbb
Quarken <ahmadsaleh3255@gmail.com>
2017-07-25 19:50:40 +0200

    False Promise health regen

--- game/dota_addons/fateanother/scripts/vscripts/abilities/nr/modifier_qgg_oracle.lua
+++ game/dota_addons/fateanother/scripts/vscripts/abilities/nr/modifier_qgg_oracle.lua
@@ -4,20 +4,21 @@ function modifier_qgg_oracle:OnCreated()
 	self.tDamageInstances = {}
 	self.fHeal = 0
 	self.hParent = self:GetParent()
+	self.fCurrentRegen = 0
+	self:StartIntervalThink(0.01)
+end
+
+function modifier_qgg_oracle:OnRefresh()
+	self.fCurrentRegen = 0
+	self:StartIntervalThink(0.01)
 end
 
 function modifier_qgg_oracle:DeclareFunctions()
 	local funcs = {
-		MODIFIER_EVENT_ON_HEAL_RECEIVED,
+		MODIFIER_PROPERTY_HEALTH_REGEN_CONSTANT,
 	}
 	
 	return funcs
-end
-
-function modifier_qgg_oracle:OnHealReceived(args)
-	if args.unit == self:GetParent() then
-		self.fHeal = self.fHeal + args.gain
-	end
 end
 
 function modifier_qgg_oracle:GetEffectName()
@@ -29,6 +30,14 @@ function modifier_qgg_oracle:GetEffectAttachType()
 end
 
 if IsServer() then
+	function modifier_qgg_oracle:OnIntervalThink()
+		self.fCurrentRegen = self:GetParent():GetHealthRegen()
+	end
+
+	function modifier_qgg_oracle:GetModifierConstantHealthRegen()
+		return 0 - self.fCurrentRegen
+	end
+	
 	function modifier_qgg_oracle:OnDestroy()
 		local hParent = self:GetParent()
 		local hAbility = self:GetAbility()
@@ -37,7 +46,13 @@ if IsServer() then
 		for k, v in pairs(self.tDamageInstances) do
 			local fDamage = CalculateDamagePreReduction(v.eDamageType, v.fDamage, hParent)
 			
-			local hSource = not v.hAbility:IsNull() and v.hAbility or hAbility
+			local hSource = nil
+			if not v.hAbility then
+				hSource = hAbility
+			else
+				hSource = v.hAbility
+			end
+			
 			if fHeal == 0 then
 				DoDamage(v.hAttacker, hParent, fDamage, v.eDamageType, 0, hSource, false)
 			end
