8f7c1cd144088821fd9c106de263cff67e7d7937
Spectrum <mattkleroy@gmail.com>
2017-07-27 13:29:03 -0700

    Nero nerf and Weakening Venom tooltip, and avenger dark passage reset
    Nero R slow reduced from 80 to 50/55/60/65/70

--- game/dota_addons/fateanother/scripts/vscripts/abilities/avenger/modifier_dark_passage.lua
+++ game/dota_addons/fateanother/scripts/vscripts/abilities/avenger/modifier_dark_passage.lua
@@ -11,6 +11,10 @@ function modifier_dark_passage:IsDebuff()
     return true
 end
 
+function modifier_dark_passage:RemoveOnDeath()
+    return false
+end
+
 function modifier_dark_passage:GetAttributes()
     return MODIFIER_ATTRIBUTE_MULTIPLE
 end
@@ -19,6 +23,15 @@ function modifier_dark_passage:OnRespawn(args)
     local hUnit = args.unit
     local hParent = self:GetParent()
     if hUnit == hParent then
+        local hAbility = hParent:FindAbilityByName("avenger_dark_passage") 
+        hAbility:EndCooldown()
+        self:Destroy()
+    end
+end
+
+function modifier_dark_passage:OnDestroy()
+    local hParent = self:GetParent()
+    if not hParent.IsAlive then
         local hAbility = hParent:FindAbilityByName("avenger_dark_passage") 
         hAbility:EndCooldown()
         self:Destroy()
